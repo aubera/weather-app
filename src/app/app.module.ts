9cfce3106b0cff81cd9104fcef64020d17e74151
Andor <andor.auber@gmail.com>
2019-06-19 16:16:58 +0200

    feat(App Module) add new components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
+import { EnvServiceProvider } from './env.service.provider';
+import { GetWeatherApiService } from './get-weather-api.service';
+import { InputSearchComponent } from './input-search/input-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+    InputSearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    EnvServiceProvider,
+    GetWeatherApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
