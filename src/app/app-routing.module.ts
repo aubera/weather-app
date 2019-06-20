b0712c0e096172a259a4f0298ec381e0c071bba1
Andor <andor.auber@gmail.com>
2019-06-20 13:59:32 +0200

    feat(app-routing.module.ts) add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ForecastComponent } from './forecast/forecast.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { WrapWeatherInputComponent } from './wrap-weather-input/wrap-weather-input.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'forecast', component: ForecastComponent},
+  {path: 'dashboard', component: WrapWeatherInputComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
